df8d69f1bff2b136f3f6e0a16d9d5fae6876b8da
Jan Steinke <jan.steinke@gmail.com>
2023-02-12 17:10:01 +0100

    add HTTP server handler

---
+++ handler/net/http/server.go
@@ -0,0 +1,43 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/jan-xyz/box"
+)
+
+func NewHTTPServer[TIn, TOut any](
+	decode func(*http.Request) (TIn, error),
+	encode func(TOut, http.ResponseWriter),
+	encodeError func(error, http.ResponseWriter),
+	endpoint box.Endpoint[TIn, TOut],
+) httpServer[TIn, TOut] {
+	return httpServer[TIn, TOut]{
+		decode:      decode,
+		encode:      encode,
+		encodeError: encodeError,
+		endpoint:    endpoint,
+	}
+}
+
+type httpServer[TIn, TOut any] struct {
+	decode      func(*http.Request) (TIn, error)
+	encode      func(TOut, http.ResponseWriter)
+	encodeError func(error, http.ResponseWriter)
+	endpoint    box.Endpoint[TIn, TOut]
+}
+
+func (s httpServer[TIn, TOut]) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	in, err := s.decode(req)
+	if err != nil {
+		s.encodeError(err, w)
+		return
+	}
+	out, err := s.endpoint(ctx, in)
+	if err != nil {
+		s.encodeError(err, w)
+		return
+	}
+	s.encode(out, w)
+}
