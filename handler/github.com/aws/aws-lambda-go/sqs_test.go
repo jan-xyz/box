b7d243f2249de6e6fe1c03e00085d7630fca3d71
Jan Steinke <jan.steinke@gmail.com>
2023-02-13 19:19:02 +0100

    add test for SQS handler

---
+++ handler/github.com/aws/aws-lambda-go/sqs_test.go
@@ -0,0 +1,111 @@
+package awslambdago
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_SQS_Handle(t *testing.T) {
+	testCases := []struct {
+		desc       string
+		fifo       bool
+		decodeFunc func(events.SQSMessage) (string, error)
+		ep         func(context.Context, string) (string, error)
+		input      *events.SQSEvent
+		want       *events.SQSEventResponse
+	}{
+		{
+			desc: "test successfully processing a single event",
+			fifo: false,
+			decodeFunc: func(events.SQSMessage) (string, error) {
+				return "", nil
+			},
+			ep: func(context.Context, string) (string, error) {
+				return "", nil
+			},
+			input: &events.SQSEvent{
+				Records: []events.SQSMessage{{}},
+			},
+			want: &events.SQSEventResponse{},
+		},
+		{
+			desc: "test failing to decode doesn't call endpoint",
+			fifo: false,
+			decodeFunc: func(ev events.SQSMessage) (string, error) {
+				return "", errors.New("boom")
+			},
+			ep: func(context.Context, string) (string, error) {
+				panic("don't call this")
+			},
+			input: &events.SQSEvent{
+				Records: []events.SQSMessage{
+					{MessageId: "first", Body: "first"},
+				},
+			},
+			want: &events.SQSEventResponse{BatchItemFailures: []events.SQSBatchItemFailure{
+				{ItemIdentifier: "first"},
+			}},
+		},
+		{
+			desc: "test failing a single event",
+			fifo: false,
+			decodeFunc: func(ev events.SQSMessage) (string, error) {
+				return ev.Body, nil
+			},
+			ep: func(_ context.Context, input string) (string, error) {
+				if input == "first" {
+					return "", errors.New("boom")
+				}
+				return "", nil
+			},
+			input: &events.SQSEvent{
+				Records: []events.SQSMessage{
+					{MessageId: "first", Body: "first"},
+					{MessageId: "second", Body: "second"},
+				},
+			},
+			want: &events.SQSEventResponse{BatchItemFailures: []events.SQSBatchItemFailure{
+				{ItemIdentifier: "first"},
+			}},
+		},
+		{
+			desc: "test failing a all events in fifo queue",
+			fifo: true,
+			decodeFunc: func(ev events.SQSMessage) (string, error) {
+				return ev.Body, nil
+			},
+			ep: func(_ context.Context, input string) (string, error) {
+				if input == "first" {
+					return "", errors.New("boom")
+				}
+				return "", nil
+			},
+			input: &events.SQSEvent{
+				Records: []events.SQSMessage{
+					{MessageId: "first", Body: "first"},
+					{MessageId: "second", Body: "second"},
+				},
+			},
+			want: &events.SQSEventResponse{BatchItemFailures: []events.SQSBatchItemFailure{
+				{ItemIdentifier: "first"},
+				{ItemIdentifier: "second"},
+			}},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			h := NewSQSHandler(
+				tC.fifo,
+				tC.decodeFunc,
+				tC.ep,
+			)
+			got := h.Handle(context.Background(), tC.input)
+
+			assert.Equal(t, tC.want, got)
+		})
+	}
+}
