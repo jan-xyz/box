283773b354fd20f9508d0e858f4fb661994885cd
Jan Steinke <jan.steinke@gmail.com>
2023-02-14 21:21:12 +0100

    add tracing middleware for sqs

--- handler/github.com/aws/aws-lambda-go/sqs.go
+++ handler/github.com/aws/aws-lambda-go/sqs.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/jan-xyz/box"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func NewSQSHandler[TIn, TOut any](
@@ -31,7 +33,7 @@ type sqsHandler[TIn, TOut any] struct {
 	endpoint box.Endpoint[TIn, TOut]
 }
 
-func (s sqsHandler[TIn, TOut]) Handle(ctx context.Context, e *events.SQSEvent) *events.SQSEventResponse {
+func (s sqsHandler[Tin, TOut]) Handle(ctx context.Context, e *events.SQSEvent) *events.SQSEventResponse {
 	resp := &events.SQSEventResponse{}
 	for _, r := range e.Records {
 		// in a FIFO queue all other items in the batch after a failure should also be failed
@@ -51,5 +53,27 @@ func (s sqsHandler[TIn, TOut]) Handle(ctx context.Context, e *events.SQSEvent) *
 			continue
 		}
 	}
+	return resp
+}
+
+func NewSQSTracingMiddleware(handler SQSHandler, tracer trace.Tracer) SQSHandler {
+	return sqsTracingMiddleware{handler: handler, tracer: tracer}
+}
+
+type sqsTracingMiddleware struct {
+	handler SQSHandler
+	tracer  trace.Tracer
+}
+
+// implementation of https://opentelemetry.io/docs/reference/specification/trace/semantic_conventions/instrumentation/aws-lambda/#sqs
+func (s sqsTracingMiddleware) Handle(ctx context.Context, e *events.SQSEvent) *events.SQSEventResponse {
+	ctx, span := s.tracer.Start(ctx, "multiple_sources process", trace.WithAttributes(
+		semconv.FaaSTriggerPubsub,
+		semconv.MessagingOperationProcess,
+		semconv.MessagingSystem("AmazonSQS"),
+		semconv.MessagingSourceKindQueue,
+	))
+	defer span.End()
+	resp := s.handler.Handle(ctx, e)
 	return resp
 }
