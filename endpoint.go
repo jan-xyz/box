3cda804bd2ea40382cd1ec9db5a5d25beb1ffe94
Jan Steinke <jan.steinke@moia.io>
2023-02-24 12:25:35 +0100

    add docs for Chain

--- endpoint.go
+++ endpoint.go
@@ -13,6 +13,8 @@ type Endpoint[TIn, TOut any] func(ctx context.Context, req TIn) (TOut, error)
 // request logging, instrumentation, context enrichment etc.
 type Middleware[TIn, TOut any] func(next Endpoint[TIn, TOut]) Endpoint[TIn, TOut]
 
+// Chain is a convenience function to chain multiple [Middleware]s together
+// and finally wraps an [Endpoint].
 func Chain[TIn, TOut any](outer Middleware[TIn, TOut], others ...Middleware[TIn, TOut]) Middleware[TIn, TOut] {
 	return func(next Endpoint[TIn, TOut]) Endpoint[TIn, TOut] {
 		for i := len(others) - 1; i >= 0; i-- { // reverse
